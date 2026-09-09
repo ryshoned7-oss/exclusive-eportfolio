c98c0dd5ed4a24f8a3748f645e0c646f03715a69
Ryshone Duncan <ryshond7@gmail.com>
2026-09-09 18:41:06 -0400

    emailjs is done

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 // template_azfdpct
 // service_oacepcj
 // NO4QBPxX4fPT7zNA9
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector('.modal__overlay--loading')
+  const success = document.querySelector('.modal__overlay--success')
+  loading.classList += ' modal__overlay--visible';
+  emailjs
+    .sendForm(
+      'service_oacepcj',
+      'template_azfdpct',
+      event.target,
+      'NO4QBPxX4fPT7zNA9'
+    ).then(() => {
+      loading.classList.remove('modal__overlay--visible')
+      success.classList += ' modal__overlay--visible'
+    }).catch(() => {
+      loading.classList.remove('modal__overlay--visible')
+      alert(
+        'The email service is temporarily unavailble. Please contact me directly at RyshoneD7@gmail.com'
+      );
+    })
+}
