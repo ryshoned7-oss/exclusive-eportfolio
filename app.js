c561cd730ceb9e9a02ec5e08d0ddce6c2c045450
Ryshone Duncan <ryshond7@gmail.com>
2026-09-10 11:55:03 -0400

    Dark Mode complete

--- app.js
+++ app.js
@@ -2,6 +2,19 @@
 // service_oacepcj
 // NO4QBPxX4fPT7zNA9
 
+let isModalOpen = false;
+let contrastToggle = false;
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += ' dark-theme'
+  }
+  else {
+    document.body.classList.remove('dark-theme')
+  }
+}
+
 function contact(event) {
   event.preventDefault();
   const loading = document.querySelector('.modal__overlay--loading')
@@ -24,7 +37,7 @@ function contact(event) {
     })
 }
 
-let isModalOpen = false;
+
 function toggleModal() {
   if (isModalOpen) {
     isModalOpen = false;
