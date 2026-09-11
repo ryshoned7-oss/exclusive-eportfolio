5655e434d623dc3c9e2770f42e25687aaa0a818e
Ryshone Duncan <ryshond7@gmail.com>
2026-09-11 03:01:52 -0400

    moving background is complete

--- app.js
+++ app.js
@@ -4,6 +4,21 @@
 
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll('.shape')
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i =0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const oddInteger = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x}px, ${y}px)`
+  }
+  /* console.log(x, y) */
+}
+
 
 function toggleContrast() {
   contrastToggle = !contrastToggle;
